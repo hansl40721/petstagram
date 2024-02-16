2ddbcdea7f593bc22f7570f5283e1b510a3daae3
Lucas Hansen <lucas.hansenst@gmail.com>
2024-02-15 21:02:34 -0600

    name changes

--- server/server.js
+++ server/server.js
@@ -18,9 +18,9 @@ const startApolloServer = async () => {
     app.use('/graphql', expressMiddleware(server))
 
     if(process.env.NODE_ENV === 'production') {
-        app.use(express.static(path.join(__dirname, '../some-project/dist')))
+        app.use(express.static(path.join(__dirname, '../client/dist')))
         app.get('*', (req, res) => {
-            res.sendFile(path.join(__dirname, '../some-project/dist/index.html'))
+            res.sendFile(path.join(__dirname, '../client/dist/index.html'))
         })
     }
 
