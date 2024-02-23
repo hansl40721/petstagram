b37be2d8febbdc3d9571a8aa1aa37b67ac2b3aba
IvanDirigible <sickeruncible@gmail.com>
2024-02-22 19:34:21 -0500

    Stashing, I guess.

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,14 +2,14 @@ import { useState } from "react";
 import { useMutation } from '@apollo/client';
 import { Link } from 'react-router-dom';
 
-import { ADD_USER } from "../utils/mutations";
-import * as Auth from '../utils/auth';
+import { LOGIN, ADD_USER } from "../utils/mutations";
+import Auth from '../utils/auth';
 import "../styles/Pages.css"
 
 import Nav from "../components/Nav";
 
 function Signup(props) {
-  const [formState, setFormState] = useState({ email: '', password: ''});
+  const [formState, setFormState] = useState({ username: '', email: '', password: ''});
   const [addUser] = useMutation(ADD_USER);
 
   const handleFormSubmit = async (event) => {
