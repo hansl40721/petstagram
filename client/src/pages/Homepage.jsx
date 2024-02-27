6c607d8b6ca76e706cfad40eac0db84b0fb26f59
IvanDirigible <sickeruncible@gmail.com>
2024-02-26 22:16:11 -0500

    Merging with main.

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -2,17 +2,15 @@ import { useState } from "react";
 import { useMutation } from '@apollo/client';
 import { Link } from 'react-router-dom';
 import { LOGIN } from '../utils/mutations';
-
 import { ADD_USER } from "../utils/mutations";
 import Auth from '../utils/auth';
 import "../styles/Pages.css"
-
 import Nav from "../components/Nav";
 
 const Homepage = () => {
     return (
     <>
-    <img src="https://res.cloudinary.com/dcjz6vl08/image/upload/c_fill,g_face,h_300,w_300/r_max/f_auto/v1708660066/myyshfx2ro3jtks99l4.jpg" />
+    <img />
     <div className="footer">
         <p>Copyright Chinchilla Group 2024</p>
     </div>
